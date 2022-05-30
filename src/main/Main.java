1decfd28abbd96a0116c10805211fe381ebe275c
unknown <agusbatti52@gmail.com>
2022-05-30 01:58:02 -0300

    Agregado prototipo de interfaz utilizando JFrames de otro IDE

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,15 @@ import Entidades.Terreno;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.Button;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -17,12 +23,16 @@ public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
         Main app = new Main();
-        app.start();
+        Prueba prueba1 = new Prueba();
+        prueba1.setVisible(true);
+        //app.start();
+        
+        
     }
 
     @Override
     public void simpleInitApp() {
-        stateManager.attach(new Engine(this));
+        //stateManager.attach(new Engine(this));
     }
 
     @Override
@@ -34,4 +44,6 @@ public class Main extends SimpleApplication {
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
     }
+    
+    
 }
