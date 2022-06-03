514a56e0f5b4696afa4940fb2b9a6c1fbc143faf
FloatBatti <agusbatti52@gmail.com>
2022-06-03 19:32:41 -0300

    Merge manual branch agus

---
+++ src/Entidades/User.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author AgusGonza
+ */
+public class User implements Serializable{
+    
+    private String userName;
+    private String password;
+    private int userId;
+    private int coins = 0;
+    private Workshop stats = new Workshop();
+    private Integer[] vehicles = new Integer[]{1,0,0,0,0,};
+    private int lastLaps = 0;
+    private transient Persistence <User> dataControl = new Persistence();
+
+    public User(String userName, String password, int lastIDplus) {
+        this.userName = userName;
+        this.password = password;
+        this.userId = lastIDplus;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+    
+    
+}
