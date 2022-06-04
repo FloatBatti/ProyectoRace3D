5fa015e70ed3c226adf232e4d51bb294e03e32ae
Fran <franciscopediconi@gmail.com>
2022-06-04 03:46:59 -0300

    clases de agus

---
+++ src/Entidades/Workshop.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author AgusGonza
+ */
+public class Workshop implements Serializable{
+    
+    private Integer[] fuelStats = new Integer[]{0,0,0,0,0};
+    private Integer[] healtStats = new Integer[]{0,0,0,0,0};
+    private Integer[] forceStats = new Integer[]{0,0,0,0,0};
+    
+}
