70db1aa114f60c77dcb97498f8e69a36779ad2c0
Barbara Basualdo <agusbatti52@gmail.com>
2022-06-05 17:28:28 -0300

    Creacion de clase User y herencias de la misma, ademas de metodos

--- src/Entidades/Workshop.java
+++ src/Entidades/Workshop.java
@@ -17,4 +17,37 @@ public class Workshop implements Serializable{
     private Integer[] healtStats = new Integer[]{0,0,0,0,0};
     private Integer[] forceStats = new Integer[]{0,0,0,0,0};
     
+    public void incremetFuel(){
+        
+        for (int i=0; i < fuelStats.length ; i++){
+            
+            if(fuelStats[i] == 0 ){
+                
+                fuelStats[i] = 1;
+            }
+        }
+    }
+    
+    public void incremetHealt(){
+        
+        for (int i=0; i < healtStats.length ; i++){
+            
+            if(healtStats[i] == 0 ){
+                
+                healtStats[i] = 1;
+            }
+        }
+    }
+        
+    public void incremetForce(){
+        
+        for (int i=0; i < forceStats.length ; i++){
+            
+            if(forceStats[i] == 0 ){
+                
+                forceStats[i] = 1;
+            }
+        }
+    }
+    
 }
